2cf3d1ce27e836664b46e09b3760b1ea3a425d47
Vivek Mehta <vivekmehta455@gmail.com>
2023-01-31 02:11:17 +0530

    Update startvm.ps1

--- startvm.ps1
+++ startvm.ps1
@@ -1,8 +1,4 @@
-Param
-(
-  [Parameter (Mandatory=$false)]
-  [String] $vmName
-)
 Connect-AzAccount -Identity
+$vmName = "vivek-test-vm"
 $resourceGroup = "vivek-automation-rg"
 Stop-AzVM -Name $vmName -ResourceGroupName $resourceGroup -Force
